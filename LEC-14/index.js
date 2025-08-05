7e93e012a6d07190ff23b0226ba9ea1066cf963a
Vrinda <vrinda7605@gmail.com>
2025-08-05 10:49:46 +0530

    lec14

---
+++ LEC-14/index.js
@@ -0,0 +1,18 @@
+const express= require("express");
+const app = express();
+const fs=require("fs");
+app.use(express.static(__dirname+"/public"));
+
+app.get("/users",(req,res)=>{
+    fs.readFile("./users.json","utf-8",function(err,data){
+        if(err) return res.send(err);
+        let users=JSON.parse(data);
+        res.json(users);
+    })
+
+})
+
+
+app.listen(3002,()=>{
+    console.log("server started")
+})
---
+++ LEC-14/public/index.js
@@ -0,0 +1,38 @@
+function getUser(URL){
+    fetch(URL)
+    .then((res)=>{
+        console.log(res);
+        return res.json();
+    })
+    .then((data)=>{
+        // console.log(data);
+        data.forEach(user => {
+            displayUser(user);
+        });
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+}
+// getUser("https://jsonplaceholder.typicode.com/users");
+getUser("http://localhost:3002/users");
+
+let userContainer = document.querySelector(".user-container")
+function displayUser(user){
+    let li = document.createElement("li");
+    li.innerHTML=`<div class="user-info">
+            <div>
+                <h1>${user.name}</h1>
+                <p>${user.username}</p>
+                <h1>${user.id}</h1>
+                <h1>${user.city}</h1>
+
+            </div>
+            <div>
+                <button>Edit</button>
+                <button>Delete</button>
+            </div>
+        </div>`
+    userContainer.appendChild(li);  
+}
+
